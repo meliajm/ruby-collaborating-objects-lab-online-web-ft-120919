68b8f1c7515fba2afd8ca5cbf00777c857accf52
melia miller <meliajm@gmail.com>
2019-12-19 05:39:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,8 +39,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    self.create_by_name(artist_name)
-    self.find_by_name(artist_name) 
+    self.find_by_name(artist_name) || self.create_by_name(artist_name)
+    
   end
   
   def print_songs
